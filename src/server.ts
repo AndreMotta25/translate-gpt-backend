415f6be21cd5cc5a57ee40e7718eba374129e87c
AndreMotta25 <andremotta16041998@gmail.com>
2023-11-21 12:32:20 -0300

    feat: add url to cors

--- src/server.ts
+++ src/server.ts
@@ -2,30 +2,32 @@ import 'express-async-errors';
 import cors from 'cors';
 import express, {NextFunction, Request, Response, json} from 'express'
 import {AuthenticationError, OpenAI} from 'openai'
+import axios from 'axios';
 
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin:"https://www.npmjs.com",
+              optionsSuccessStatus: 200}));
 app.use(json());
 
 app.post("/translate", async (request:Request, response: Response) => {
   const {text, apiKey: key, org} = request.body; 
-
+  console.log('aqui')
   const api = new OpenAI({
     apiKey: key,
     organization:org
   }) 
 
   const content = await api.chat.completions.create({
+    model:'gpt-3.5-turbo',
     messages:[
       {role:"system",content:"You will receive a text in English and your task is to translate it into Brazilian Portuguese. Be consistent and understand the context. Do not translate proper names or nouns. Try not to be redundant. If there are html tags, keep them "},
       {role:"user", content: text}],
-    model:'gpt-3.5-turbo'
-  })
-
-  return response.json({message: content.choices[0].message.content});
-})
+    })
+    console.log(content.choices[0].message.content)
+    return response.json({message: content.choices[0].message.content})
+});  
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if(err instanceof AuthenticationError) return response.status(401).json({msg: err.message})
