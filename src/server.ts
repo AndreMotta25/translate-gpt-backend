85b8964ac3274b66bb962642731cddf43856cc48
AndreMotta25 <andremotta16041998@gmail.com>
2023-12-05 21:12:36 -0300

    feat: add translate-stream endpoint

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,18 @@ import cors from 'cors';
 import express, {NextFunction, Request, Response, json} from 'express'
 import {AuthenticationError, OpenAI} from 'openai'
 import axios from 'axios';
-
+import {Readable} from 'node:stream'
+import { OpenAIStream, StreamingTextResponse, streamToResponse } from 'ai-stream-experimental'
 
 const app = express();
+export const runtime = 'edge'
 
 app.use(cors());
 app.use(json());
 
 app.post("/translate", async (request:Request, response: Response) => {
-  const {text, apiKey: key, org} = request.body; 
   console.log('aqui')
+  const {text, apiKey: key, org} = request.body; 
   const api = new OpenAI({
     apiKey: key,
     organization:org
@@ -24,12 +26,31 @@ app.post("/translate", async (request:Request, response: Response) => {
       {role:"system",content:"You will receive a text in English and your task is to translate it into Brazilian Portuguese. Be consistent and understand the context. Do not translate proper names or nouns. Try not to be redundant. If there are html tags, keep them "},
       {role:"user", content: text}],
     })
-    console.log(content.choices[0].message.content)
     return response.json({message: content.choices[0].message.content})
+});  
+app.post("/translate-stream", async (request:Request, response: Response) => {
+  console.log('aqui')
+  const {text, apiKey: key, org} = request.body; 
+
+  const api = new OpenAI({
+    apiKey: key,
+  }) 
+
+  const content = await api.chat.completions.create({
+    model:'gpt-3.5-turbo',
+    messages:[
+      {role:"system",content:"You will receive a text in English and your task is to translate it into Brazilian Portuguese. Be consistent and understand the context. Do not translate proper names or nouns. Try not to be redundant. If there are html tags, keep them "},
+      {role:"user", content: text}],
+      stream:true
+    })
+
+    const stream = OpenAIStream(content);
+    streamToResponse(stream, response)
 });  
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if(err instanceof AuthenticationError) return response.status(401).json({msg: err.message})
+  console.log(err)
   return response.status(500).json({msg: err.message})
 })
 
